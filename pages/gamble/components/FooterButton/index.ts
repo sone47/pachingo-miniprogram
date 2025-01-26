b2e25e844762a4f241717024498787a286a62c8d
Sone <sonezeng@tencent.com>
2025-01-27 03:36:56 +0900

    complete gambling user interaction

---
+++ pages/gamble/components/FooterButton/index.ts
@@ -0,0 +1,51 @@
+// pages/gamble/components/FooterButton/index.ts
+Component({
+
+  /**
+   * Component properties
+   */
+  properties: {
+    gamblingChipCount: {
+      type: Number,
+      value: 0,
+    },
+    lottoryResultList: {
+      type: Array,
+      value: [],
+    }
+  },
+
+  /**
+   * Component initial data
+   */
+  data: {
+    hasChip: false,
+    hasPlayed: false,
+  },
+
+  observers: {
+    lottoryResultList() {
+      this.setData({
+        hasPlayed: !!this.data.lottoryResultList.length,
+      })
+    },
+    gamblingChipCount() {
+      this.setData({
+        hasChip: !!this.data.gamblingChipCount,
+      })
+    },
+  },
+
+  /**
+   * Component methods
+   */
+  methods: {
+    handlePrintButtonTap() {
+      this.triggerEvent('print')
+    },
+
+    handlePlayButtonTap() {
+      this.triggerEvent('play')
+    }
+  }
+})
--- pages/gamble/gamble.ts
+++ pages/gamble/gamble.ts
@@ -1,6 +1,7 @@
 Page({
   data: {
-    gamblingChipCount: 3
+    gamblingChipCount: 3,
+    lottoryResultList: [], // DesireItem[][]
   },
 
   onShow() {
@@ -9,7 +10,50 @@ Page({
     wx.setNavigationBarTitle({ title: '今日TODO' })
   },
 
-  handlePlayButtonTap() {
-    console.log('抽取心愿')
+  async handlePrint() {
+    // TODO 选中心愿
   },
+
+  async handlePlay() {
+    const lotteryResult = await this.getLotteryResult()
+    const gamblingChipCount = await this.getNewChipCount()
+     this.setData({
+      lottoryResultList: [...this.data.lottoryResultList, lotteryResult],
+      gamblingChipCount,
+     })
+  },
+
+  async getNewChipCount() {
+    return this.data.gamblingChipCount - 1
+  },
+
+  async getLotteryResult() {
+    return [
+      {
+        id: 1,
+        name: '吃柿子甜品',
+        description: '店子在福田~再不吃可能就过季了没得吃了！！警惕警惕警惕~~~~~',
+        priority: 4,
+        startTime: '2024-06-07',
+        endTime: '2024-07-08',
+        createTime: '2024-12-12'
+      },
+      {
+        id: 2,
+        name: '吃柿子甜品',
+        description: '店子在福田~再不吃可能就过季了没得吃了！！警惕警惕警惕~~~~~',
+        priority: 4,
+        endTime: '2024-07-08',
+        createTime: '2024-12-01'
+      },
+      {
+        id: 3,
+        name: '吃柿子甜品',
+        description: '店子在福田~再不吃可能就过季了没得吃了！！警惕警惕警惕~~~~~',
+        priority: 4,
+        endTime: '2024-07-08',
+        createTime: '2024-12-01'
+      },
+    ]
+  }
 })
